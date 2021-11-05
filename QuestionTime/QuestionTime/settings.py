3c49dfb7a8c3f3d23c84801bae98286439f029ef
MarianMaschkeRGST <m.maschke@rgst.net>
2021-11-05 18:08:44 +0900

    added customer user/admin and login/auth templates

--- QuestionTime/QuestionTime/settings.py
+++ QuestionTime/QuestionTime/settings.py
@@ -37,6 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+
+    'django_registration',
+    'djoser',
+    'widget_tweaks',
+
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +132,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.CustomUser'
