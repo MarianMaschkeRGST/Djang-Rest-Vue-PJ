89cf4395e3dce7b79ff057e7bedb65edcc5ee94a
MarianMaschkeRGST <m.maschke@rgst.net>
2021-11-13 18:37:55 +0900

    added django registration form

--- QuestionTime/QuestionTime/urls.py
+++ QuestionTime/QuestionTime/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/register/',
         RegistrationView.as_view(form_class=CustomUserForm, success_url='/'),
-        name='django_registration_register'),
+        name='django_registration_register',
+    ),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
