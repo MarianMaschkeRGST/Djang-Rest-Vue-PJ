6bf6af12290fb5b9c972796d2716c219a628c259
MarianMaschkeRGST <m.maschke@rgst.net>
2021-11-15 11:49:50 +0900

    added token auth and started app questions

--- QuestionTime/QuestionTime/urls.py
+++ QuestionTime/QuestionTime/urls.py
@@ -26,5 +26,11 @@ urlpatterns = [
         RegistrationView.as_view(form_class=CustomUserForm, success_url='/'),
         name='django_registration_register',
     ),
+    
     path('accounts/', include('django.contrib.auth.urls')),
+
+    path("api-auth/", include("rest_framework.urls")),
+
+    path("auth/", include("djoser.urls")),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
