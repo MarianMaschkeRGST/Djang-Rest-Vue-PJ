f6c8f8b3a82923d45a128b1ea9ad14a9f67bfd0f
MarianMaschkeRGST <m.maschke@rgst.net>
2021-11-21 15:18:36 +0900

    added VUE CLi

--- QuestionTime/questions/api/urls.py
+++ QuestionTime/questions/api/urls.py
@@ -26,4 +26,10 @@ urlpatterns = [
         qv.AnswerRUDAPIView.as_view(),
         name="answer-detail",
         ),
+
+    path(
+        "answers/<uuid:uuid>/like/", 
+        qv.AnswerLikeAPIView.as_view(),
+        name="answer-like",
+        ),
 ]
