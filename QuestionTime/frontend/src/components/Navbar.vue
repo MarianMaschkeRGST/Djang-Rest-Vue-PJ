e5f26d86c57969fb811f2e6dfa0555d1633a764d
MarianMaschkeRGST <m.maschke@rgst.net>
2021-11-24 10:39:42 +0900

    added Vue navbar components

---
+++ QuestionTime/frontend/src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto">
+                <li>
+                    <a class="btn btn-outline-success nav-item" href="/accounts/logout/">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+</template>
+
+<script>
+export default {
+    name: "Navbar",
+};
+</script>
